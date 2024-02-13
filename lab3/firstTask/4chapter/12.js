cfbed27c70711aa20e8696c049a0afd1097a2370
Gaziza Akhmetzhan <gaziza@MacBook-Air-Gaziza.local>
2024-02-14 01:08:12 +0600

    commit

---
+++ lab3/firstTask/4chapter/12.js
@@ -0,0 +1,12 @@
+function camelize(str) {
+    return str
+      .split('-') 
+      .map(
+        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+      )
+      .join('');
+  }
+
+camelize("background-color"); 
+camelize("list-style-image"); 
+camelize("-webkit-transition");
---
+++ lab3/secondTask/script.js
@@ -0,0 +1,28 @@
+function addItem() {
+    var input = document.getElementById("todoInput");
+    var inputValue = input.value.trim();
+    if (inputValue === "") return;
+  
+    var ul = document.getElementById("todoList");
+    var li = document.createElement("li");
+    li.textContent = inputValue;
+  
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function() {
+      li.style.textDecoration = this.checked ? "line-through" : "none";
+    });
+  
+    var deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.onclick = function() {
+      ul.removeChild(li);
+    };
+  
+    li.appendChild(checkbox);
+    li.appendChild(deleteButton);
+    ul.appendChild(li);
+  
+    input.value = "";
+  }
+  
